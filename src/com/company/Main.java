b7c2d1b619ac335d7cfb3fd27bc62c0118fe1464
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 14:51:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        int c = (a + 5) * ++b;
+        System.out.println(c);  
+    }
+}
